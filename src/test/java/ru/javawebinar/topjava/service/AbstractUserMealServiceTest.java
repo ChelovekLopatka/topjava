9bdd59154d69f3ad92cb9285ffc7c5989ed1b656
ChelovekLopatka <golimb111@gmail.com>
2016-04-16 12:32:13 +0300

    lesson06 patch03

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserMealServiceTest.java
@@ -5,17 +5,8 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.MealTestData;
-import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -28,15 +19,7 @@ import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringJUnit4ClassRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(Profiles.HSQLDB)
-public class UserMealServiceTest {
-    private static final Logger LOG = LoggerFactory.getLogger(UserMealServiceTest.class);
+abstract public class AbstractUserMealServiceTest extends AbstractServiceTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -45,7 +28,7 @@ public class UserMealServiceTest {
     // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
     public Stopwatch stopwatch = new Stopwatch() {
         private void logInfo(Description description, long nanos) {
-            LOG.info(String.format("+++ Test %s spent %d ms",
+            log.info(String.format("+++ Test %s spent %d ms",
                     description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos)));
         }
 
